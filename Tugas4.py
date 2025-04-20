016c08469adb9346a399372cd43389258fd0d6b0
atharikputra <144183038+atharikputra@users.noreply.github.com>
2025-04-20 23:58:19 +0700

    Add files via upload

---
+++ Tugas4.py
@@ -0,0 +1,98 @@
+# =====================================================================================================
+# Membangun sebuah model Neural Network untuk klasifikasi dataset Horse or Human dalam binary classes.
+#
+# Input layer harus menerima 150x150 dengan 3 bytes warna sebagai input shapenya.
+# Jangan menggunakan lambda layers dalam model.
+#
+# Dataset yang digunakan dibuat oleh Laurence Moroney (laurencemoroney.com).
+#
+# Standar yang harus dicapai untuk accuracy dan validation_accuracy > 83%
+# =====================================================================================================
+
+import urllib.request
+import zipfile
+import tensorflow as tf
+import os
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+
+def solution_05():
+    data_url_1 = 'https://github.com/dicodingacademy/assets/releases/download/release-horse-or-human/horse-or-human.zip'
+    urllib.request.urlretrieve(data_url_1, 'horse-or-human.zip')
+    local_file = 'horse-or-human.zip'
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall('data/horse-or-human')
+
+    data_url_2 = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/validation-horse-or-human.zip'
+    urllib.request.urlretrieve(data_url_2, 'validation-horse-or-human.zip')
+    local_file = 'validation-horse-or-human.zip'
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall('data/validation-horse-or-human')
+    zip_ref.close()
+
+    TRAINING_DIR = 'data/horse-or-human'
+    train_datagen = ImageDataGenerator(
+        rescale=1/255,
+        rotation_range=40,
+        horizontal_flip=True,
+        shear_range=0.2,
+        zoom_range=0.2,
+        fill_mode='nearest'
+    )
+
+    train_generator= ImageDataGenerator(rescale=1./255)
+    
+    # YOUR CODE HERE
+    train_generator = train_datagen.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+
+    VALIDATION_DIR='data/validation-horse-or-human'
+
+     # Hanya rescale untuk validation set 
+    validation_datagen=ImageDataGenerator(rescale=1./255)
+
+    validation_generator=validation_datagen.flow_from_directory(
+        VALIDATION_DIR,
+        target_size=(150 , 150),
+        batch_size=32 ,
+        class_mode='binary')
+
+    model=tf.keras.models.Sequential([
+        # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid') #DO NOT CHANGE THIS LINE!
+        ])
+    
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer=RMSprop(learning_rate=0.001),
+        metrics=['accuracy']
+    )
+
+    model.fit(
+        train_generator,
+        epochs=10,
+        validation_data=validation_generator,
+        verbose=1
+    )
+
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model=solution_05()
+    model.save("model_05.h5")
